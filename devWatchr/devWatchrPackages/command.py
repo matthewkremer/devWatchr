a15346605127a0de0fbce7e7c52d1df8826eda16
Matthew Kremer <mkremer90@gmail.com>
2013-07-26 13:30:54 -0500

    Update command.py

--- devWatchr/devWatchrPackages/command.py
+++ devWatchr/devWatchrPackages/command.py
@@ -1,7 +1,11 @@
 import pyinotify, subprocess, devWatchr
 
 '''
-
+	Executes a linux command on the system. Settings include the default settings and:
+	{
+		'command': A string representing a command line command that will be executed.
+		'cwd': The current working directory that the command should be run using.
+	}
 '''
 
 class command(devWatchr.watcher):
